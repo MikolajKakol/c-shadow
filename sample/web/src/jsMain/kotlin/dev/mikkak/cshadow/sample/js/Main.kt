64807eb708ace936395c4db2d81e288124e610a6
Mikołaj Kąkol <mikolaj.kakol@gmail.com>
2025-05-12 23:33:54 +0200

    clean up js sample

--- sample/web/src/jsMain/kotlin/dev/mikkak/cshadow/sample/js/Main.kt
+++ sample/web/src/jsMain/kotlin/dev/mikkak/cshadow/sample/js/Main.kt
@@ -1,29 +1,16 @@
 package dev.mikkak.cshadow.sample.js
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import dev.mikkak.cshadow.sample.BoxShadowExample
+import kotlinx.browser.document
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-  onWasmReady {
-    ComposeViewport(
-      viewportContainerId = "Sample",
-    ) {
-//      PageLoadNotify()
-      BoxShadowExample()
+    onWasmReady {
+        ComposeViewport(document.body!!) {
+            BoxShadowExample()
+        }
     }
-  }
-}
-
-external fun onLoadFinished()
-
-@Composable
-fun PageLoadNotify() {
-  LaunchedEffect(Unit) {
-    onLoadFinished()
-  }
 }
